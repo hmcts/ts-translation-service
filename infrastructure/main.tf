381d94134f8d49df6d41e58602ab7575d80d4ef6
markdathorne <mark.dathorne@cgi.com>
2022-04-26 11:52:04 +0100

    RDM-13170 Construct/configure new Translation Service skeleton project

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,29 @@
 provider "azurerm" {
   features {}
 }
+
+module "ts-${var.env}" {
+  source              = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  product             = var.product
+  env                 = var.env
+  tenant_id           = var.tenant_id
+  object_id           = var.jenkins_AAD_objectId
+  resource_group_name = azurerm_resource_group.rg.name
+  product_group_name  = "ts_translate"
+  common_tags         = var.common_tags
+  create_managed_identity    = true
+}
+
+resource "azurerm_key_vault_secret" "AZURE_APPINSGHTS_KEY" {
+  name         = "AppInsightsInstrumentationKey"
+  value        = azurerm_application_insights.appinsights.instrumentation_key
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_application_insights" "appinsights" {
+  name                = "${var.product}-${var.env}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+  tags                = var.common_tags
+}
