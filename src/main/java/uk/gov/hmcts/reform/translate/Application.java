14126970916cc3cc3f3f56633deb73c24056a469
markdathorne <mark.dathorne@cgi.com>
2022-04-27 14:57:02 +0100

    RDM-13170 Trigger build

--- src/main/java/uk/gov/hmcts/reform/translate/Application.java
+++ src/main/java/uk/gov/hmcts/reform/translate/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
+@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, it's not a utility class
 public class Application {
 
     public static void main(final String[] args) {
