a5add66a11dcbe4138533c39f06733ccdb0d28ce
Gareth Sullivan <gareth.sullivan@cgi.com>
2022-05-05 16:09:14 +0100

    WLTS-7 - Add new entities and repositories

---
+++ src/main/java/uk/gov/hmcts/reform/translate/data/DictionaryEntity.java
@@ -0,0 +1,37 @@
+package uk.gov.hmcts.reform.translate.data;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Table(name = "dictionary")
+@Entity
+@NoArgsConstructor
+@Data
+public class DictionaryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY,
+        generator = "dictionary_id_seq")
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "english_phrase")
+    private String englishPhrase;
+
+    @Column(name = "translation_phrase")
+    private String translationPhrase;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "translation_version", referencedColumnName = "version")
+    private TranslationUploadEntity translationUpload;
+}
