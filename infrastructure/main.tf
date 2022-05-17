b6814e84c11144d245af7672ce97ed5994c4296f
Gareth Sullivan <gareth.sullivan@cgi.com>
2022-05-17 17:57:38 +0100

    WLTS-14 - Add s2s vault resources

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -93,3 +93,19 @@ resource "azurerm_key_vault_secret" "POSTGRES-DATABASE" {
   value        = module.ts-translation-service-db.postgresql_database
   key_vault_id = module.key-vault.key_vault_id
 }
+
+data "azurerm_key_vault" "s2s_vault" {
+  name                = "s2s-${var.env}"
+  resource_group_name = "rpe-service-auth-provider-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "ts_translation_service_s2s_key" {
+  name         = "microservicekey-ts-translation-service"
+  key_vault_id = data.azurerm_key_vault.s2s_vault.id
+}
+
+resource "azurerm_key_vault_secret" "ts_translation_service_s2s_secret" {
+  name         = "ts-translation-service-s2s-secret"
+  value        = data.azurerm_key_vault_secret.ts_translation_service_s2s_key.value
+  key_vault_id = module.vault.key_vault_id
+}
