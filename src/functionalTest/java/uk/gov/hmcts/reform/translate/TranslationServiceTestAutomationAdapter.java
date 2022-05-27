58c2cbce680b35e9654f9c5b20c63eaf26c93869
Gareth Sullivan <gareth.sullivan@cgi.com>
2022-05-27 12:02:34 +0100

    WLTS-18 - Address code review comments

--- src/functionalTest/java/uk/gov/hmcts/reform/translate/TranslationServiceTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/translate/TranslationServiceTestAutomationAdapter.java
@@ -17,39 +17,45 @@ public class TranslationServiceTestAutomationAdapter extends DefaultTestAutomati
     public synchronized Object calculateCustomValue(BackEndFunctionalTestScenarioContext scenarioContext, Object key) {
 
         if (key.toString().startsWith("containsDictionaryTranslations")) {
-            try {
-                @SuppressWarnings("unchecked")
-                Map<String, String> translations = (Map<String, String>) ReflectionUtils.deepGetFieldInObject(
-                    scenarioContext,
-                    "testData.actualResponse.body.translations"
-                );
-
-                String expectedValueStr = key.toString().replace("containsDictionaryTranslations ", "");
-
-                Map<String, String> expectedValuesMap = new HashMap<>();
-
-                Arrays.stream(expectedValueStr.split(",")).toList().forEach(s -> {
-                    String[] keyValuePair = s.split(":");
-                    expectedValuesMap.put(keyValuePair[0].trim(),
-                                          keyValuePair[1].trim());
-                });
-
-
-                boolean allMatch = expectedValuesMap.entrySet().stream().allMatch(entrySet ->
-                    translations.containsKey(entrySet.getKey()) && translations.containsValue(entrySet.getValue())
-                );
-
-                if (allMatch) {
-                    return translations;
-                } else {
-                    return expectedValueStr;
-                }
-            } catch (Exception e) {
-                throw new FunctionalTestException("Problem checking acceptable response payload: ", e);
-            }
+            return assertDictionaryTranslationsContainsExpectedPhrases(scenarioContext, key);
         }
 
         return super.calculateCustomValue(scenarioContext, key);
+    }
+
+    private Object assertDictionaryTranslationsContainsExpectedPhrases(
+        BackEndFunctionalTestScenarioContext scenarioContext, Object key) {
+        try {
+            @SuppressWarnings("unchecked")
+            Map<String, String> translations = (Map<String, String>) ReflectionUtils.deepGetFieldInObject(
+                scenarioContext,
+                "testData.actualResponse.body.translations"
+            );
+
+            String expectedValueStr = key.toString().replace("containsDictionaryTranslations ", "");
+
+            Map<String, String> expectedValuesMap = new HashMap<>();
+
+            Arrays.stream(expectedValueStr.split(",")).toList().forEach(s -> {
+                String[] keyValuePair = s.split(":");
+                expectedValuesMap.put(keyValuePair[0].trim(),
+                                      keyValuePair[1].trim());
+            });
+
+
+            boolean allMatch = expectedValuesMap.entrySet().stream().allMatch(
+                entrySet ->translations.containsKey(entrySet.getKey())
+                    && translations.containsValue(entrySet.getValue())
+            );
+
+            if (allMatch) {
+                return translations;
+            } else {
+                return expectedValueStr;
+            }
+        } catch (Exception e) {
+            throw new FunctionalTestException("Problem checking acceptable response payload: ", e);
+        }
     }
 
     @Override
