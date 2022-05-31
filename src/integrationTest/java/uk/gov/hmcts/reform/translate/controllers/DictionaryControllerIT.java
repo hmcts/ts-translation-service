bd439866dec0f1fd502ec871e3c2174e507fcc1c
Semenu Aggor <semenu.aggor@HMCTS.NET>
2022-05-31 22:54:58 +0100

    WLTS-24 [WIP] Refactor DB schema and entities

--- src/integrationTest/java/uk/gov/hmcts/reform/translate/controllers/DictionaryControllerIT.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/translate/controllers/DictionaryControllerIT.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.translate.controllers;
 
 import com.pivovarit.function.ThrowingSupplier;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -8,10 +9,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.web.util.NestedServletException;
 import uk.gov.hmcts.reform.translate.BaseTest;
 
 import java.util.LinkedHashMap;
@@ -24,11 +25,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.hmcts.reform.translate.model.ControllerConstants.DICTIONARY_URL;
@@ -66,19 +66,6 @@ public class DictionaryControllerIT extends BaseTest {
                 .andExpect(status().is(200))
                 .andExpect(jsonPath("$.translations", equalTo(expectedDictionary)))
                 .andReturn();
-        }
-
-        @Test
-        @Sql(scripts = {DELETE_TRANSLATION_TABLES_SCRIPT, GET_TRANSLATION_TABLES_DUPLICATE_ENGLISH_PHRASES_SCRIPT})
-        void shouldThrowExceptionWhenDictionaryReturnsDuplicateEnglishPhrases() {
-            NestedServletException nestedServletException = assertThrows(
-                NestedServletException.class,
-                () -> mockMvc.perform(get(DICTIONARY_URL)
-                                          .contentType(APPLICATION_JSON_VALUE))
-            );
-
-            assertTrue(nestedServletException.getCause() instanceof IllegalStateException);
-            assertTrue(nestedServletException.getCause().getMessage().contains("Duplicate key English Phrase 1"));
         }
 
         @Test
@@ -120,7 +107,7 @@ public class DictionaryControllerIT extends BaseTest {
 
         @Test
         @Sql(scripts = {DELETE_TRANSLATION_TABLES_SCRIPT})
-        void shouldTest() {
+        void shouldTestConcurrentAddToDictionaryViaTranslateEndpoint() {
             final String payload = "{\"phrases\": [\"English phrase 2\"]}";
 
             final CompletableFuture<ResultActions> future1 = CompletableFuture.supplyAsync(
@@ -139,9 +126,48 @@ public class DictionaryControllerIT extends BaseTest {
             final Throwable thrown = catchThrowable(() -> CompletableFuture.allOf(future1, future2).get());
 
             // THEN
-            assertThat(thrown)
-                .isNotNull()
-                .isInstanceOf(ExecutionException.class);
+            assertThatDataIntegrityViolationExceptionWasThrown(thrown);
         }
+    }
+
+
+    @Nested
+    @Disabled
+    class PutDictionary {
+
+        @Test
+        @Sql(scripts = {DELETE_TRANSLATION_TABLES_SCRIPT})
+        void shouldTestConcurrentAddToDictionaryViaPutEndpoint() {
+            final String payload = "{\"English phrase 2\": \"Translated Phrase 2\","
+                + " \"English Phrase 3\": \"Translated Phrase 3\"}";
+
+            final CompletableFuture<ResultActions> future1 = CompletableFuture.supplyAsync(
+                ThrowingSupplier.unchecked(() -> mockMvc.perform(put(DICTIONARY_URL)
+                                                                     .contentType(APPLICATION_JSON_VALUE)
+                                                                     .accept(APPLICATION_JSON_VALUE)
+                                                                     .content(payload)))
+            );
+            final CompletableFuture<ResultActions> future2 = CompletableFuture.supplyAsync(
+                ThrowingSupplier.unchecked(() -> mockMvc.perform(put(DICTIONARY_URL)
+                                                                     .contentType(APPLICATION_JSON_VALUE)
+                                                                     .accept(APPLICATION_JSON_VALUE)
+                                                                     .content(payload)))
+            );
+
+            final Throwable thrown = catchThrowable(() -> CompletableFuture.allOf(future1, future2).get());
+
+            // THEN
+            assertThatDataIntegrityViolationExceptionWasThrown(thrown);
+        }
+    }
+
+    private void assertThatDataIntegrityViolationExceptionWasThrown(final Throwable thrown) {
+        assertThat(thrown)
+            .isNotNull()
+            .isInstanceOfSatisfying(
+                ExecutionException.class,
+                exception -> assertThat(exception.getCause().getCause().getCause())
+                    .isInstanceOf(DataIntegrityViolationException.class)
+            );
     }
 }
--- src/main/resources/db/migration/V20220527_24__WLTS-24-AddUniqueConstraintTo-Dictionary-EnglishPhrase.sql
+++ src/main/resources/db/migration/V20220527_24__WLTS-24-AddUniqueConstraintTo-Dictionary-EnglishPhrase.sql
@@ -2,20 +2,3 @@
 
 ALTER TABLE ONLY public.dictionary
     ADD CONSTRAINT english_phrase_unique UNIQUE (english_phrase);
-
---BEGIN;
---  ALTER TABLE ONLY public.dictionary ALTER id DROP DEFAULT; -- drop default
---
---  DROP SEQUENCE public.dictionary_id_seq;              -- drop owned sequence
---
---  ALTER TABLE ONLY public.dictionary
---     ALTER id ADD GENERATED ALWAYS AS IDENTITY (RESTART 1);
---COMMIT;
---
---
---
---
---ALTER TABLE ONLY public.dictionary
---    ALTER COLUMN id TYPE BIGSERIAL GENERATED ALWAYS AS IDENTITY PRIMARY KEY;
---
---DROP SEQUENCE public.dictionary_id_seq;
