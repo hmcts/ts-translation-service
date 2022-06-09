609c8f0fd5d28de4564c963aa213490032e5c805
cameronguthrie <cameron.guthrie@HMTCS.NET>
2022-06-09 17:23:45 +0100

    WLTS-17 TranslationServiceTestAutomationAdapter update

--- src/functionalTest/java/uk/gov/hmcts/reform/translate/TranslationServiceTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/translate/TranslationServiceTestAutomationAdapter.java
@@ -10,14 +10,30 @@ import uk.gov.hmcts.befta.util.ReflectionUtils;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class TranslationServiceTestAutomationAdapter extends DefaultTestAutomationAdapter {
+
+    private String uniqueEnglishPhrase;
 
     @Override
     public synchronized Object calculateCustomValue(BackEndFunctionalTestScenarioContext scenarioContext, Object key) {
 
         if (key.toString().startsWith("containsDictionaryTranslations")) {
             return assertDictionaryTranslationsContainsExpectedPhrases(scenarioContext, key);
+        }
+
+        if (key.toString().startsWith("createUniqueTranslationPhrase")) {
+            return createUniqueTranslationPhrase(key);
+        }
+
+        if (key.toString().startsWith("createUniqueEnglishPhrase")) {
+            return createUniqueEnglishPhrase();
+        }
+
+        if (key.toString().startsWith("containsUniqueDictionaryTranslations")) {
+            String expectedValueStr = key.toString().replace("containsUniqueDictionaryTranslations ", "");
+            return assertDictionaryTranslationsContainsExpectedPhrases(scenarioContext,uniqueEnglishPhrase+": "+ expectedValueStr);
         }
 
         return super.calculateCustomValue(scenarioContext, key);
@@ -53,6 +69,29 @@ public class TranslationServiceTestAutomationAdapter extends DefaultTestAutomati
             } else {
                 return expectedValueStr;
             }
+        } catch (Exception e) {
+            throw new FunctionalTestException("Problem checking acceptable response payload: ", e);
+        }
+    }
+
+    private Object createUniqueTranslationPhrase(Object key) {
+        try {
+            String expectedValueStr = key.toString().replace("createUniqueTranslationPhrase ", "");
+            Map<String, String> translations = new HashMap<>();
+            uniqueEnglishPhrase = UUID.randomUUID().toString();
+            translations.put(uniqueEnglishPhrase, expectedValueStr);
+            return translations;
+        } catch (Exception e) {
+            throw new FunctionalTestException("Problem checking acceptable response payload: ", e);
+        }
+    }
+
+    private Object createUniqueEnglishPhrase() {
+        try {
+            Map<String, String> translations = new HashMap<>();
+            uniqueEnglishPhrase = UUID.randomUUID().toString();
+            translations.put(uniqueEnglishPhrase, "");
+            return translations;
         } catch (Exception e) {
             throw new FunctionalTestException("Problem checking acceptable response payload: ", e);
         }
