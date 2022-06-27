c13a3acc2a0b165f56e2f20ebd60d2e204ce6116
Matt Nayler <matt.nayler@cgi.com>
2022-06-27 17:57:16 +0100

    WLTS-34 too many versions generated for updates
       [WLTS-34](https://tools.hmcts.net/jira/browse/WLTS-34)
    
    Multiple translation updates submitted in a single upload should all use
     the same upload record.

--- src/main/java/uk/gov/hmcts/reform/translate/service/DictionaryService.java
+++ src/main/java/uk/gov/hmcts/reform/translate/service/DictionaryService.java
@@ -14,6 +14,7 @@ import uk.gov.hmcts.reform.translate.errorhandling.RoleMissingException;
 import uk.gov.hmcts.reform.translate.helper.DictionaryMapper;
 import uk.gov.hmcts.reform.translate.model.Dictionary;
 import uk.gov.hmcts.reform.translate.repository.DictionaryRepository;
+import uk.gov.hmcts.reform.translate.repository.TranslationUploadRepository;
 import uk.gov.hmcts.reform.translate.security.SecurityUtils;
 
 
@@ -40,15 +41,18 @@ public class DictionaryService {
     private final DictionaryMapper dictionaryMapper;
     private final SecurityUtils securityUtils;
     private final ApplicationParams applicationParams;
+    private final TranslationUploadRepository translationUploadRepository;
 
     @Autowired
     public DictionaryService(DictionaryRepository dictionaryRepository, DictionaryMapper dictionaryMapper,
-                             SecurityUtils securityUtils, ApplicationParams applicationParams) {
+                             SecurityUtils securityUtils, ApplicationParams applicationParams,
+                             TranslationUploadRepository translationUploadRepository) {
 
         this.dictionaryRepository = dictionaryRepository;
         this.dictionaryMapper = dictionaryMapper;
         this.securityUtils = securityUtils;
         this.applicationParams = applicationParams;
+        this.translationUploadRepository = translationUploadRepository;
     }
 
     public Map<String, String> getDictionaryContents() {
@@ -103,23 +107,27 @@ public class DictionaryService {
         validateDictionary(dictionaryRequest, isManageTranslationRole);
         val currentUserId = securityUtils.getUserInfo().getUid();
 
-        val translationUploadOptional = hasAnyTranslations(dictionaryRequest)
+        val translationUploadEntity = hasAnyTranslations(dictionaryRequest)
             ? dictionaryMapper.createTranslationUploadEntity(currentUserId)
             : null;
 
+        // if upload entity has been generated save it now as we know we have at least one translation that will use it
+        if (translationUploadEntity != null) {
+            translationUploadRepository.save(translationUploadEntity);
+        }
+
         dictionaryRequest.getTranslations().entrySet()
-            .stream()
-            .forEach(phrase -> processPhrase(phrase, translationUploadOptional));
+            .forEach(phrase -> processPhrase(phrase, translationUploadEntity));
     }
 
     private void processPhrase(Map.Entry<String, String> currentPhrase,
-                               TranslationUploadEntity translationUploadOptional) {
+                               TranslationUploadEntity translationUploadEntity) {
 
         val result = dictionaryRepository.findByEnglishPhrase(currentPhrase.getKey());
         if (result.isPresent()) {
-            updatePhrase(currentPhrase, result.get(), translationUploadOptional);
+            updatePhrase(currentPhrase, result.get(), translationUploadEntity);
         } else {
-            createNewPhrase(currentPhrase, translationUploadOptional);
+            createNewPhrase(currentPhrase, translationUploadEntity);
         }
     }
 
