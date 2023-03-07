33ea3f53fe30ee905bbd95a0b939f764d9606647
Rajaram <70951210+rajaram-nagarajan@users.noreply.github.com>
2023-03-07 10:29:02 +0000

    removed changes

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -1,17 +1,3 @@
 terraform {
   backend "azurerm" {}
-  
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.45.0"       # AzureRM provider version
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "1.6.0"
-    }
-  }
 }
