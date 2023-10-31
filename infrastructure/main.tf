57b600b87268c1b98159d1a6c6f507ffead9e5ae
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2023-10-31 11:05:44 +0000

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -109,3 +109,50 @@ resource "azurerm_key_vault_secret" "ts_translation_service_s2s_secret" {
   value        = data.azurerm_key_vault_secret.ts_translation_service_s2s_key.value
   key_vault_id = module.key-vault.key_vault_id
 }
+
+module "postgresql_v15" {
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  providers = {
+    azurerm.postgres_network = azurerm.postgres_network
+  }
+
+  admin_user_object_id = var.jenkins_AAD_objectId
+  business_area        = "cft"
+  common_tags          = var.common_tags
+  component            = var.component
+  env                  = var.env
+  pgsql_databases = [
+    {
+      name = var.database_name
+    }
+  ]
+  pgsql_server_configuration = [
+    {
+      name  = "azure.extensions"
+      value = "plpgsql,pg_stat_statements,pg_buffercache,hypopg"
+    }
+  ]
+  pgsql_version    = "15"
+  product          = var.product
+  name             = "${local.app_full_name}-postgres-db-v15"
+  pgsql_sku        = var.pgsql_sku
+  pgsql_storage_mb = var.pgsql_storage_mb
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-USER-V15" {
+  name         = "${var.component}-POSTGRES-USER-V15"
+  value        = module.postgresql_v15.username
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-PASS-V15" {
+  name         = "${var.component}-POSTGRES-PASS-V15"
+  value        = module.postgresql_v15.password
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-HOST-V15" {
+  name         = "${var.component}-POSTGRES-HOST-V15"
+  value        = module.postgresql_v15.fqdn
+  key_vault_id = module.key-vault.key_vault_id
+}
