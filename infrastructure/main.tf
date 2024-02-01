35b773dfa7a04ad9c3827e2f0dc9b38cfd55f897
Muhammad <muhammad.umerji@hmcts.net>
2024-02-01 15:12:15 +0000

    Test - DO NOT MERGE

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -67,6 +67,7 @@ module "ts-translation-service-db" {
   backup_retention_days = var.backup_retention_days
   georedundant_backup   = var.georedundant_backup
   common_tags           = var.common_tags
+  count                 = contains(["demo", "ithc", "perftest", "aat"], var.env) ? 0 : 1
 }
 
 ////////////////////////////////
@@ -75,32 +76,37 @@ module "ts-translation-service-db" {
 
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
   name         = "${var.component}-POSTGRES-USER"
-  value        = module.ts-translation-service-db.user_name
+  value        = module.ts-translation-service-db[0].user_name
   key_vault_id = module.key-vault.key_vault_id
+  count        = contains(["demo", "ithc", "perftest", "aat", "prod"], var.env) ? 0 : 1
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   name         = "${var.component}-POSTGRES-PASS"
-  value        = module.ts-translation-service-db.postgresql_password
+  value        = module.ts-translation-service-db[0].postgresql_password
   key_vault_id = module.key-vault.key_vault_id
+  count        = contains(["demo", "ithc", "perftest", "aat", "prod"], var.env) ? 0 : 1
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-HOST" {
   name         = "${var.component}-POSTGRES-HOST"
-  value        = module.ts-translation-service-db.host_name
+  value        = module.ts-translation-service-db[0].host_name
   key_vault_id = module.key-vault.key_vault_id
+  count        = contains(["demo", "ithc", "perftest", "aat", "prod"], var.env) ? 0 : 1
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PORT" {
   name         = "${var.component}-POSTGRES-PORT"
-  value        = module.ts-translation-service-db.postgresql_listen_port
+  value        = module.ts-translation-service-db[0].postgresql_listen_port
   key_vault_id = module.key-vault.key_vault_id
+  count        = contains(["demo", "ithc", "perftest", "aat", "prod"], var.env) ? 0 : 1
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-DATABASE" {
   name         = "${var.component}-POSTGRES-DATABASE"
-  value        = module.ts-translation-service-db.postgresql_database
+  value        = module.ts-translation-service-db[0].postgresql_database
   key_vault_id = module.key-vault.key_vault_id
+  count        = contains(["demo", "ithc", "perftest", "aat", "prod"], var.env) ? 0 : 1
 }
 
 data "azurerm_key_vault" "s2s_vault" {
