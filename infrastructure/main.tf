5e4f5d526acc7ecae386104ba457550a74477ef4
Muhammad <muhammad.umerji@hmcts.net>
2024-03-12 12:38:33 +0000

    Updating vault reference

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -106,7 +106,7 @@ module "postgresql_v15" {
   enable_schema_ownership = var.enable_schema_ownership
   force_schema_ownership_trigger = "1"
   kv_subscription = var.kv_subscription
-  kv_name = data.azurerm_key_vault.ts-${var.env}.name
+  kv_name = data.azurerm_key_vault.ts-${var.env}
   user_secret_name = azurerm_key_vault_secret.POSTGRES-USER.name
   pass_secret_name = azurerm_key_vault_secret.POSTGRES-PASS.name
 }
