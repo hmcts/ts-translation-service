cc9f1ed8a9219264c59f2432ed386b6be1b3cde5
54687 <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
2024-07-04 12:05:21 +0000

    Updating Terraform Formatting

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -7,18 +7,18 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {}
-  
+
   required_providers {
-     azurerm = {
-       source  = "hashicorp/azurerm"
-       version = "~> 3.110.0"       # AzureRM provider version
-     }
-     random = {
-       source = "hashicorp/random"
-     }
-     azuread = {
-       source  = "hashicorp/azuread"
-       version = "2.53.1"
-     }
-   }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.110.0" # AzureRM provider version
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.53.1"
+    }
+  }
 }
