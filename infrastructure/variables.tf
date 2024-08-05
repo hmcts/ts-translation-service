5f5b6344fd1e46f021695f67b1b8a25e4028bfb7
Ben Lang <132359359+lang-ben@users.noreply.github.com>
2024-08-05 11:40:44 +0100

    Remove refs to hmcts/cnp-module-postgres

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -31,7 +31,6 @@ variable "jenkins_AAD_objectId" {
 // Database
 ////////////////////////////////
 
-// Define the default values for optional parameters (see https://github.com/hmcts/cnp-module-postgres)
 variable "sku_name" {
   default = "GP_Gen5_2"
 }
@@ -59,8 +58,6 @@ variable "backup_retention_days" {
 variable "georedundant_backup" {
   default = "Enabled"
 }
-
-// Define the values for mandatory/required parameters (see https://github.com/hmcts/cnp-module-postgres)
 
 variable "postgresql_user" {
   default = "ts"
