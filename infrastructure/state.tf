531ff35e25ae8b1bd805a03d8d6f5c90fc555e94
Sammi Chong <shinyee.chong@hmcts.net>
2025-01-06 16:39:35 +0000

    Update state.tf

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0.0"
+      version = "~> 4.0"
     }
     random = {
       source = "hashicorp/random"
